7773efee559f768fa64fddd56ed2fe94f81fe7fd
Michael Li <michael.li.work@outlook.com>
2023-11-04 00:15:40 +0800

    new: page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,7 +1,14 @@
+import Head from "next/head";
 import React from "react";
 
 const projects = () => {
-  return <div>projects</div>;
+  return (
+    <>
+      <Head>
+        <title>Michael Li | Projects</title>
+      </Head>
+    </>
+  );
 };
 
 export default projects;
